e5245f530c2d6b2be544437c60a151351430ee91
Jiaqi Sheng <jqsheng94@hotmail.com>
2016-11-01 11:07:20 -0700

    add comments

--- fetch videos from channel.py
+++ fetch videos from channel.py
@@ -6,9 +6,7 @@ import os.path
 import csv
 
 
-
-
-print( "=========================== list of Youtube videos ================================")
+rint( "=========================== list of Youtube videos ================================")
 
 
 YT_channel_ids = ['UCmf7DwvMqFi3kLPMLIANIrg']     # Add ChannelIds in here
@@ -22,9 +20,6 @@ YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
 
-
-
-
 videos_by_channel = {}
 
 
@@ -33,7 +28,7 @@ for channel_id in YT_channel_ids:
                                             type='video',
                                             order='date',    # can be changed to 'viewcount'
                                             channelId=channel_id,
-                                            maxResults=30).execute()
+                                            maxResults=30).execute()  #30 is the number of videos to extract from the channel
     videoIds=[]
     for search_result in search_response.get("items", []):
         if search_result["id"]["kind"] == "youtube#video":
@@ -41,7 +36,7 @@ for channel_id in YT_channel_ids:
     print(videoIds)
 
 
-
+# After get the video Ids, save them into a csv file in the folder.
 
     file_exists = os.path.isfile("/PUT THE PATH HERE/video.csv")
     with open("/PUT THE PATH HERE/video.csv", 'a') as f:
